d04da0e069526b907f6b3c62aace805086c34578
unknown <moistrack999@gmail.com>
2020-04-17 15:59:52 -0700

    Primer Cambio Proyecto

---
+++ Demostraccion/src/main/java/com/mycompany/demostraccion/newSQLTemplate.sql
@@ -0,0 +1,10 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  MoisesA
+ * Created: Apr 2, 2020
+ */
+
